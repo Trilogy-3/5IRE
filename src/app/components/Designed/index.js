b63643fe290b5d625718e9c310d89eeb69039ed2
piyushverma26 <piyushv528@gmail.com>
2024-10-19 13:45:40 +0530

    Changes in the build

--- src/app/components/Designed/index.js
+++ src/app/components/Designed/index.js
@@ -1,50 +1,54 @@
-"use client";
-import { useState, useEffect } from 'react';
-import DesignedCauroselItem from './components/DesignedCaurosel';
-import { data } from './components/data/CauroselData';
+'use client'
+import { useState, useEffect } from 'react'
+import DesignedCauroselItem from './components/DesignedCaurosel'
+import { data } from './components/data/CauroselData'
 
 export default function Designed() {
-    const [activeIndex, setActiveIndex] = useState(0);
-    if (data.length < 1 || !data) return null;
+  const [activeIndex, setActiveIndex] = useState(0)
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setActiveIndex((prev) => (prev + 1) % data.length);
-        }, 3000);
+  useEffect(() => {
+    if (data.length > 0) {
+      const interval = setInterval(() => {
+        setActiveIndex((prev) => (prev + 1) % data.length)
+      }, 3000)
 
-        return () => clearInterval(interval);
-    }, [data.length]);
+      return () => clearInterval(interval)
+    }
+  }, [])
 
-    return (
-        <div className="mt-12 flex flex-row items-center">
-            <div className="flex-grow w-full">
-                <div className="transition-opacity duration-500 ease-in-out opacity-100">
-                    <DesignedCauroselItem
-                        btnColor={data[activeIndex].btnColor}
-                        videoLink={data[activeIndex].videoLink}
-                        btnHref={data[activeIndex].btnHref}
-                        btnValue={data[activeIndex].btnValue}
-                        title={data[activeIndex].title}
-                        desc={data[activeIndex].desc}
-                        isPercent={data[activeIndex].isPercent}
-                        percentValue={data[activeIndex].percent?.value}
-                        sibling={data[activeIndex].percent?.sibling}
-                        percentName={data[activeIndex].percent?.name}
-                        borderColor={data[activeIndex].borderColor}
-                    />
-                </div>
-            </div>
-            <div className='max-w-max flex flex-col gap-2 items-center'>
-                {data.map((_, index) => (
-                    <Indiacator key={index} cssMain={activeIndex === index ? 'h-8 bg-[#8B57E3]' : 'bg-white'} />
-                ))}
-            </div>
+  if (!data || data.length < 1) return null
+
+  return (
+    <div className='mt-12 flex flex-row items-center'>
+      <div className='flex-grow w-full'>
+        <div className='transition-opacity duration-500 ease-in-out opacity-100'>
+          <DesignedCauroselItem
+            btnColor={data[activeIndex].btnColor}
+            videoLink={data[activeIndex].videoLink}
+            btnHref={data[activeIndex].btnHref}
+            btnValue={data[activeIndex].btnValue}
+            title={data[activeIndex].title}
+            desc={data[activeIndex].desc}
+            isPercent={data[activeIndex].isPercent}
+            percentValue={data[activeIndex].percent?.value}
+            sibling={data[activeIndex].percent?.sibling}
+            percentName={data[activeIndex].percent?.name}
+            borderColor={data[activeIndex].borderColor}
+          />
         </div>
-    );
+      </div>
+      <div className='max-w-max flex flex-col gap-2 items-center'>
+        {data.map((_, index) => (
+          <Indicator
+            key={index}
+            cssMain={activeIndex === index ? 'h-8 bg-[#8B57E3]' : 'bg-white'}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
-function Indiacator({ cssMain = 'bg-white' }) {
-    return (
-        <div className={`h-4 w-2 rounded-full ${cssMain}`}></div>
-    );
+function Indicator({ cssMain = 'bg-white' }) {
+  return <div className={`h-4 w-2 rounded-full ${cssMain}`}></div>
 }
--- src/app/components/HomeLanding/components/HeaderText/index.js
+++ src/app/components/HomeLanding/components/HeaderText/index.js
@@ -1,67 +1,71 @@
-"use client";
+'use client'
 
-import { useRef, useEffect } from "react";
-import gsap from "gsap";
+import { useRef, useEffect, useMemo } from 'react'
+import gsap from 'gsap'
 
 export default function HeaderText() {
-  const titleRef = useRef(null);
-  const titles = ["Real World Actions", "Sustainability", "AI"];
+  const titleRef = useRef(null)
+
+  const titles = useMemo(
+    () => ['Real World Actions', 'Sustainability', 'AI'],
+    []
+  )
 
   function SplitText(text) {
     return text
-      .split("")
+      .split('')
       .map((char, index) => `<span key=${index}>${char}</span>`)
-      .join("");
+      .join('')
   }
 
   useEffect(() => {
-    let i = 0;
+    let i = 0
 
     const animateText = () => {
-      const clutter = SplitText(titles[i]);
-      titleRef.current.innerHTML = clutter;
+      const clutter = SplitText(titles[i])
+      titleRef.current.innerHTML = clutter
 
-      const characters = titleRef.current.children;
+      const characters = titleRef.current.children
 
-      
+      // GSAP animation for staggered text appearance
       gsap.from(characters, {
-        x: -100,  
+        x: -100,
         duration: 0.1,
         stagger: {
           amount: 1,
-          from: "start",  
+          from: 'start',
         },
         opacity: 0,
-      });
+      })
 
       setTimeout(() => {
         gsap.to(characters, {
-          x: 100,  
+          x: 100,
           duration: 0.2,
           stagger: {
             amount: 1,
-            from: "end", 
+            from: 'end',
           },
           opacity: 0,
           onComplete: () => {
-            i = (i + 1) % titles.length;
-            animateText();
+            i = (i + 1) % titles.length
+            animateText()
           },
-        });
-      }, 2000);  
-    };
+        })
+      }, 2000)
+    }
 
-    animateText();
+    animateText()
 
-    return () => clearTimeout();
-  }, [titles]);
+    return () => clearTimeout()
+  }, [titles])
 
   return (
     <span
-      className="bg-title-gradient bg-clip-text text-transparent"
+      className='bg-title-gradient bg-clip-text text-transparent'
       ref={titleRef}
     >
       {titles[0]}
     </span>
-  );
+  )
 }
